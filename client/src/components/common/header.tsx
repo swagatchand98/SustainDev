e080a5fdd90885f3fd58616a62797cf5b681d499
Swagat Chand <swagatsaswatchand@gmail.com>
2025-02-17 17:30:19 +0530

    made the navbar responsive but not added the interactivity yet

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -1,16 +1,18 @@
+import { IoMdMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <div className="fixed w-screen h-17 bg-transparent flex justify-center items-center py-15">
-      <div className="flex justify-between items-center w-350 h-17 bg-green-100 bg-opacity-15 rounded-full px-30">
+    <div className="fixed w-screen h-12 xs:h-17 bg-transparent flex justify-center items-center py-10 xs:py-15">
+      <div className="flex justify-between items-center w-90 xs:w-350 h-12 xs:h-17 bg-green-100 bg-opacity-15 rounded-full px-5 xs:px-30">
         <div className="flex items-center">
-          <img className="w-48" src="/src/assets/logo.png" alt="sustaindev" />
+          <img className="w-30 xs:w-48" src="/src/assets/logo.png" alt="sustaindev" />
         </div>
 
         <div className="">
           <nav>
-            <ul className="flex justify-between items-center text-2xl font-judson font-extralight">
+            <ul className="hidden xs:flex justify-between items-center text-2xl font-judson font-extralight">
               <li className="px-5">
                 <Link to={"/"}>Home</Link>
               </li>
@@ -30,8 +32,12 @@ const Header: React.FC = () => {
           </nav>
         </div>
 
-        <div className="flex justify-center w-40 h-8 rounded-full text-xl font-extralight items-center border-1 border-black font-judson">
+        <div className="hidden xs:flex justify-center w-40 h-8 rounded-full text-xl font-extralight items-center border-1 border-black font-judson">
           <Link to={"/login"}>Sign Up / Log in</Link>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <IoMdMenu size={25}/>
         </div>
       </div>
     </div>
