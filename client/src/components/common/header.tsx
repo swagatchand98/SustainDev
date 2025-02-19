c726e17fa5cfb54e22994e56ab98126b425ddab6
Swagat Chand <swagatsaswatchand@gmail.com>
2025-02-19 18:38:20 +0530

    added a dropdown navbar in the header for mobile users

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -1,25 +1,38 @@
 import { IoMdMenu } from "react-icons/io";
-//import { RxCross2 } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import IsScrolledContext from "../../context/IsScrolledContext";
+import { NavBarContext } from "../../context/navBarContext";
 import { motion } from "motion/react";
 import { useContext } from "react";
 
 const Header: React.FC = () => {
-
   const { isScrolled } = useContext(IsScrolledContext);
+  const { isNavBarOpen, toggleNavBar } = useContext(NavBarContext);
 
   return (
-    <div className="fixed min-w-screen h-12 md:h-15 xl:h-17 bg-transparent flex justify-center items-center pt-15 xs:pt-10 sm:pt-7 md:pt-10 xl:py-15 ">
+    <div
+      className={`transform transition-transform duration-1000 ease-in-out ${
+        isNavBarOpen
+          ? "fixed min-w-screen h-60 md:h-60 xl:h-60 bg-transparent flex justify-center items-center pt-15 xs:pt-10 sm:pt-7 md:pt-10 xl:py-15"
+          : "fixed min-w-screen h-12 md:h-15 xl:h-17 bg-transparent flex justify-center items-center pt-15 xs:pt-10 sm:pt-7 md:pt-10 xl:py-15"
+      }`}
+    >
       <motion.div
-        className="flex justify-between items-center w-90 xs:w-120 md:w-200 xl:w-350 h-12 md:h-15 xl:h-17 bg-opacity-15 rounded-full px-5 md:px-10 xl:px-30 z-50"
+        className={`transform transition-transform duration-1000 ease-in-out ${
+          isNavBarOpen
+            ? "items-start rounded-4xl w-90 xs:w-120 md:w-200 xl:w-350 h-60 md:h-60 xl:h-60 bg-opacity-50 pt-3 px-5 md:px-10 xl:px-30 z-50"
+            : "flex justify-between items-center rounded-full  w-90 xs:w-120 md:w-200 xl:w-350 h-12 md:h-15 xl:h-17 bg-opacity-15 px-5 md:px-10 xl:px-30 z-50"
+        }`}
         initial={{ backgroundColor: "#82EE7030" }}
         animate={{
-          boxShadow: isScrolled
-            ? "0 8px 12px -2px rgb(0 0 0 / 0.2), 0 4px 8px -4px rgb(0 0 0 / 0.2)"
-            : "none",
-          backdropFilter: isScrolled ? "blur(8px)" : "none",
-          backgroundColor: isScrolled ? "#82EE7050" : "#82EE7030",
+          boxShadow:
+            isScrolled || isNavBarOpen
+              ? "0 8px 12px -2px rgb(0 0 0 / 0.2), 0 4px 8px -4px rgb(0 0 0 / 0.2)"
+              : "none",
+          backdropFilter: isScrolled || isNavBarOpen ? "blur(8px)" : "none",
+          backgroundColor:
+            isScrolled || isNavBarOpen ? "#82EE7050" : "#82EE7030",
         }}
         transition={{ duration: 0.3 }}
       >
@@ -33,38 +46,64 @@ const Header: React.FC = () => {
 
         <div className="">
           <nav>
-            <motion.ul className="hidden xl:flex justify-between items-center text-2xl font-judson font-extralight"
-            whileHover={{ scale: 1.02}}
+            <motion.ul
+              className={`${
+                isNavBarOpen
+                  ? "items-center text-lg lg:text-xl xl:text-2xl pt-5 font-judson font-extralight"
+                  : "hidden xl:flex justify-between items-center text-2xl font-judson font-extralight"
+              }`}
+              whileHover={{ scale: 1.02 }}
             >
-              <motion.li className="px-5" whileHover={{scale:1.1}}>
+              <motion.li className="px-5" whileHover={{ scale: 1.1 }}>
                 <Link to={"/"}>Home</Link>
               </motion.li>
 
-              <motion.li className="px-5" whileHover={{scale:1.1}}>
+              <motion.li className="px-5" whileHover={{ scale: 1.1 }}>
                 <Link to={"/services"}>Services</Link>
               </motion.li>
 
-              <motion.li className="px-5" whileHover={{scale:1.1}}>
+              <motion.li className="px-5" whileHover={{ scale: 1.1 }}>
                 <Link to={"/about-us"}>About-us</Link>
               </motion.li>
 
-              <motion.li className="px-5" whileHover={{scale:1.1}}>
+              <motion.li className="px-5" whileHover={{ scale: 1.1 }}>
                 <Link to={"/contacts"}>Contacts</Link>
               </motion.li>
             </motion.ul>
           </nav>
         </div>
 
-        <motion.div className="hidden xl:flex justify-center w-40 h-8 rounded-full text-xl font-extralight items-center font-judson"
-        initial={{border:'1px solid black'}}
-        whileHover={{backgroundColor: '#1D820C', scale: 1.1, color: 'white', border: 'none'}}
-        transition={{duration: 0.2}}
+        <motion.div
+          className={`${
+            isNavBarOpen
+              ? "flex justify-center w-30 h-7 md:w-40 xl:h-8 m-3 rounded-full text-sm md:text-lg xl:text-xl font-extralight items-center font-judson"
+              : "hidden xl:flex justify-center w-40 h-8 rounded-full text-xl font-extralight items-center font-judson"
+          }`}
+          initial={{ border: "1px solid black" }}
+          whileHover={{
+            backgroundColor: "#1D820C",
+            scale: 1.1,
+            color: "white",
+            border: "none",
+          }}
+          transition={{ duration: 0.2 }}
         >
           <Link to={"/login"}>Sign Up / Log in</Link>
         </motion.div>
 
-        <div className="flex justify-center items-center xl:hidden">
-          <IoMdMenu className="text-2xl md:text-3xl" />
+        <div
+          onClick={toggleNavBar}
+          className={`${
+            isNavBarOpen
+              ? "flex justify-end items-center relative -top-52 xl:hidden"
+              : "flex justify-center items-center xl:hidden"
+          }`}
+        >
+          {isNavBarOpen ? (
+            <RxCross2 className="text-2xl md:text-3xl" />
+          ) : (
+            <IoMdMenu className="text-2xl md:text-3xl" />
+          )}
         </div>
       </motion.div>
     </div>
