a63183a263828b22710e65059d5f2dd79ad87189
Swagat Chand <swagatsaswatchand@gmail.com>
2025-02-25 20:53:28 +0530

    made the signup and login page slightly more more responsive in mobiles

--- client/src/screens/loginScreen.tsx
+++ client/src/screens/loginScreen.tsx
@@ -11,17 +11,17 @@ const LoginScreen: React.FC = () => {
 
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-green-gradient-1 to bg-green-gradient-2">
-      <div className="flex-col place-items-center w-85 s:w-100 xs:w-130 h-120 p-5 rounded-2xl bg-green-100 shadow-2xl">
+      <div className="flex-col place-items-center w-70 s:w-80 xs:w-130 h-90 xs:h-120 p-5 rounded-2xl bg-green-100 shadow-2xl">
         {/* top of the signup comp */}
         <div className="relative flex justify-center items-center w-full cursor-pointer">
           <div
             onClick={() => navigate(-1)}
             className="absolute top-0 left-0 flex col-span-1"
           >
-            <HiOutlineArrowLeft className="text-2xl" />
+            <HiOutlineArrowLeft className="text-xl xs:text-2xl" />
           </div>
 
-          <div className="text-2xl font-judson font-bold tracking-widest">
+          <div className="text-xl xs:text-2xl font-judson font-bold tracking-widest">
             Login
           </div>
         </div>
@@ -29,13 +29,13 @@ const LoginScreen: React.FC = () => {
         <div className="h-[1px] bg-gray-500 w-full my-3"></div>
 
         {/* form part of the signup comp */}
-        <div className="flex justify-center items-center py-22">
-          <form className="flex-col space-y-5">
+        <div className="flex justify-center items-center py-14 xs:py-22">
+          <form className="flex-col space-y-3 xs:space-y-5">
             <div>
               <input
                 type="email"
                 placeholder="your email"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson"
               />
             </div>
 
@@ -43,7 +43,7 @@ const LoginScreen: React.FC = () => {
               <input
                 type={eyeOpen ? "text" : "password"}
                 placeholder="password"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson pr-10"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson pr-10"
               />
               {eyeOpen ? (
                 <FaEye
@@ -63,16 +63,16 @@ const LoginScreen: React.FC = () => {
         </div>
 
         {/* sumbit part of the signup comp */}
-        <div className="flex-col justify-items-start w-75 s:w-90 xs:w-100">
+        <div className="flex-col justify-items-start w-60 s:w-70 xs:w-100">
           <div className="flex items-center gap-x-4">
-            <div className="flex justify-center text-white items-center text-2xl font-judson w-25 h-10 rounded-lg bg-green-500">
+            <div className="flex justify-center text-white items-center text-xl xs:text-2xl font-judson w-20 xs:w-25 h-10 rounded-lg bg-green-500">
               <Link to={"#"}>Submit</Link>
             </div>
 
-            <FcGoogle className="text-4xl rounded-full bg-white p-1" />
+            <FcGoogle className="text-3xl xs:text-4xl rounded-full bg-white p-1" />
           </div>
 
-          <div className="text-lg font-judson font-light pt-2">
+          <div className="text-sm xs:text-lg font-judson font-light pt-2">
             <Link to={"/signup"}>Don't have an account ?</Link>
           </div>
         </div>
--- client/src/screens/signupScreen.tsx
+++ client/src/screens/signupScreen.tsx
@@ -10,17 +10,17 @@ const SignupScreen: React.FC = () => {
 
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-green-gradient-1 to bg-green-gradient-2">
-      <div className="flex-col place-items-center w-85 s:w-100 xs:w-130 h-120 p-5 rounded-2xl bg-green-100 shadow-2xl">
+      <div className="flex-col place-items-center w-70 s:w-80 xs:w-130 h-90 xs:h-120 p-4 xs:p-5 rounded-2xl bg-green-100 shadow-2xl">
         {/* top of the signup comp */}
         <div className="relative flex justify-center items-center w-full cursor-pointer">
           <div
             onClick={() => navigate(-1)}
             className="absolute top-0 left-0 col-span-1"
           >
-            <HiOutlineArrowLeft className="text-2xl" />
+            <HiOutlineArrowLeft className="text-xl xs:text-2xl" />
           </div>
 
-          <div className="col-span-2 text-2xl font-judson font-bold tracking-widest">
+          <div className="col-span-2 text-xl xs:text-2xl font-judson font-bold tracking-widest">
             SignUp
           </div>
 
@@ -30,13 +30,13 @@ const SignupScreen: React.FC = () => {
         <div className="h-[1px] bg-gray-500 w-full my-3"></div>
 
         {/* form part of the signup comp */}
-        <div className="flex justify-center items-center py-10">
-          <form className="flex-col space-y-5">
+        <div className="flex justify-center items-center py-4 xs:py-10">
+          <form className="flex-col space-y-2 xs:space-y-5">
             <div>
               <input
                 type="text"
                 placeholder="your username"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson"
               />
             </div>
 
@@ -44,7 +44,7 @@ const SignupScreen: React.FC = () => {
               <input
                 type="email"
                 placeholder="your email"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson"
               />
             </div>
 
@@ -52,7 +52,7 @@ const SignupScreen: React.FC = () => {
               <input
                 type={eyeOpen ? "text" : "password"}
                 placeholder="password"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson"
               />
               {eyeOpen ? (
                 <FaEye
@@ -73,7 +73,7 @@ const SignupScreen: React.FC = () => {
               <input
                 type={eyeOpen ? "text" : "password"}
                 placeholder="confirm password"
-                className="w-75 s:w-90 xs:w-100 bg-white h-10 rounded-lg px-2 text-lg font-judson"
+                className="w-60 s:w-70 xs:w-100 bg-white h-10 rounded-lg px-2 text-sm xs:text-lg font-judson"
               />
               {eyeOpen ? (
                 <FaEye
@@ -93,16 +93,16 @@ const SignupScreen: React.FC = () => {
         </div>
 
         {/* sumbit part of the signup comp */}
-        <div className="flex-col justify-items-start w-75 s:w-90 xs:w-100">
+        <div className="flex-col justify-items-start w-60 s:w-70 xs:w-100">
           <div className="flex items-center gap-x-4">
-            <div className="flex justify-center text-white items-center text-2xl font-judson w-25 h-10 rounded-lg bg-green-500">
+            <div className="flex justify-center text-white items-center text-xl xs:text-2xl font-judson w-20 xs:w-25 h-10 rounded-lg bg-green-500">
               <Link to={"#"}>Submit</Link>
             </div>
 
-            <FcGoogle className="text-4xl rounded-full bg-white p-1" />
+            <FcGoogle className="text-3xl xs:text-4xl rounded-full bg-white p-1" />
           </div>
 
-          <div className="text-lg font-judson font-light pt-2">
+          <div className="text-sm xs:text-lg font-judson font-light pt-1 xs:pt-2">
             <Link to={"/Login"}>Already have an account ?</Link>
           </div>
         </div>
