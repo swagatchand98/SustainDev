25cdf6977243600075ea4c7ae36d176b2d9eb723
Swagat Chand <swagatsaswatchand@gmail.com>
2025-03-03 03:06:47 +0530

    updated the user schema

--- server/src/db/users.ts
+++ server/src/db/users.ts
@@ -2,6 +2,51 @@ import mongoose from "mongoose";
 
 const schema = mongoose.Schema;
 
+const coin = new schema({
+  coin: Number,
+  transictionTime: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const wasteCollection = new schema({
+  plastic: Number,
+  paper: Number,
+  metal: Number,
+  glass: Number,
+  eWaste: Number,
+  textile: Number,
+});
+
+const userAddress = new schema({
+  house: {
+    type: String,
+    required: [true, "house no. or building name is required !"],
+    trim: true
+  },
+  area: {
+    type: String,
+    required: [true, "Area name or any landmark is required !"],
+    trim: true
+  },
+  town: {
+    type: String,
+    required: [true, "Town or village name is required !"],
+    trim: true
+  },
+  state: {
+    type: String,
+    required: [true, "state name is required !"],
+    trim: true
+  },
+  pincode: {
+    type: Number,
+    required: [true, "Pincode is required !"],
+    trim: true
+  },
+})
+
 const userSchema = new schema({
   username: {
     type: String,
@@ -29,7 +74,12 @@ const userSchema = new schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  wallet: [coin],
+  wasteCollectionHistory: [wasteCollection],
+  adress: userAddress
+
+
 });
 
 const userModel = mongoose.model("users", userSchema);
