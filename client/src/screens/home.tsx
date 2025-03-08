08dcbdd0b345fd36f015f87e7ea0040303aa1999
Swagat Chand <swagatsaswatchand@gmail.com>
2025-03-08 22:25:09 +0530

    updated home.tsx

--- client/src/screens/home.tsx
+++ client/src/screens/home.tsx
@@ -1,25 +1,31 @@
+import { lazy, Suspense } from "react";
 import Header from "../components/common/header";
-import Hero from "../components/home/hero";
-import ConfirmOrder from "../components/home/confirmOrder";
-import Services from "../components/home/services";
-import ContactUs from "../components/home/contactUs";
 import { IsScrolledContextProvider } from "../context/IsScrolledContext";
 import HeroImageContextProvider from "../context/heroImageContext";
 import NavBarContextProvider from "../context/navBarContext";
 import { Outlet } from "react-router-dom";
 
+const Hero = lazy(() => import("../components/home/hero"));
+const ConfirmOrder = lazy(() => import("../components/home/confirmOrder"));
+const Services = lazy(() => import("../components/home/services"));
+const ContactUs = lazy(() => import("../components/home/contactUs"));
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-b from-green-gradient-2 to-green-gradient-1">
       <IsScrolledContextProvider>
       <HeroImageContextProvider>  
       <NavBarContextProvider>
           <Header />
-          <Hero />
-          <ConfirmOrder />
-          <Services />
-          <ContactUs />
-          <Outlet/>
+          <Suspense fallback={<div>Loading...</div>}>
+            <main>
+              <Hero />
+              <ConfirmOrder />
+              <Services />
+              <ContactUs />
+            </main>
+            <Outlet/>
+          </Suspense>
       </NavBarContextProvider>
       </HeroImageContextProvider>
       </IsScrolledContextProvider>
